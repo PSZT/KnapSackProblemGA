e6a3460cb87d20133d4cde63b1c5831bfa8cde38
wojtek960506 <you@example.com>
2018-05-11 12:48:58 +0200

    fitness & read from file

--- KnapSack/src/main/java/Chromosome.java
+++ KnapSack/src/main/java/Chromosome.java
@@ -1,50 +1,67 @@
 import java.security.SecureRandom;
+import java.util.List;
 
 public class Chromosome {
     int size;
-    byte [] genes;
+    int [] genes;
     int chrTotalVolume;
     int chrTotalBenefit;
-
-
-    int capacityOfKnapsack; //zeby nie wywalalo bledu. To jest calkowity rozmiar plecaka w funkcji fitnessCalculate
+    int capacityOfKnapsack=5; //zeby nie wywalalo bledu. To jest calkowity rozmiar plecaka w funkcji fitnessCalculate
 
     static SecureRandom rand;
 
+    public int[] getGenes() {
+        return genes;
+    }
+
     public Chromosome(int size) {
         this.rand = new SecureRandom();
-        rand.nextBytes(genes);
+        //rand.nextBytes(genes);
         this.size = size;
-        this.genes = new byte[size];
-        for (int i:genes) {
-            genes[i] = 0;
+        this.genes = new int[size];
+
+        for(int i:genes) {
+            i = 0;
         }
+
+        /*for (int i=0 ; i < genes.length ; i++) {
+            genes[i] = 0;
+        }*/
+
+        this.chrTotalBenefit = 0;
+        this.chrTotalVolume = 0;
     }
 
     void generateChromosome() {
-        rand.nextBytes(genes);
+        for (int i=0 ; i<genes.length ; i++) {
+            genes[i] = rand.nextInt(2);
+        }
+
     }
 
-    int fitnessCalculate(Item [] items) {
+    int fitnessCalculate(List<Item> items) {
 
         for (int i=0 ; i < size ; i++) {
 
             //if current item is included in knapsack, add its volume and benefit
             //to the total volume and benefit
             if(genes[i] == 1) {
-                chrTotalVolume += items[i].getVolume();
-                chrTotalBenefit += items[i].getBenefit();
+                chrTotalVolume += items.get(i).getVolume();
+                chrTotalBenefit += items.get(i).getBenefit();
             }
 
-            
             if( chrTotalVolume > capacityOfKnapsack ) {
                 int n;
                 //Randomly choose items from the chromosome
                 //until we generate an item that is included in the knapsack
                 do {
-                    n = rand.nextInt(size - 1);
+                    n = rand.nextInt(size);
                 } while (genes[n]==0);
                 genes[n] = 0;
+                chrTotalVolume = 0;
+                chrTotalBenefit = 0;
+                System.out.println("mama");
+                i = -1; //we have to check all items again
             }
         }
         return chrTotalBenefit;
