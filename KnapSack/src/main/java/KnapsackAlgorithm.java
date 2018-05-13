3b1b6018a5a82a6a8f211ac686300262f5646383
micst96 <md.stawiarski@gmail.com>
2018-05-13 09:49:59 +0200

    group selection

--- KnapSack/src/main/java/KnapsackAlgorithm.java
+++ KnapSack/src/main/java/KnapsackAlgorithm.java
@@ -1,5 +1,42 @@
-import java.util.List;
+import java.util.*;
 
 public class KnapsackAlgorithm {
     List<Item> itemList;
+
+
+
+
+    public Chromosome groupSelection(ArrayList<Chromosome> chr) {
+
+
+        Chromosome chosenChromosome;
+        int randomChromosomeIndex;
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(99)+1;
+
+        if (randomNumber >= 0 && randomNumber < 50) {
+
+            randomChromosomeIndex = rand.nextInt(chr.size() / 4);
+            chosenChromosome = chr.get(randomChromosomeIndex);
+
+
+        } else if (randomNumber >= 50 && randomNumber < 80) {
+
+            randomChromosomeIndex = rand.nextInt((chr.size() / 2) - chr.size() / 4) + chr.size() / 4;
+            chosenChromosome = chr.get(randomChromosomeIndex);
+
+
+        }else if (randomNumber >= 80 && randomNumber < 95) {
+
+            randomChromosomeIndex = rand.nextInt((chr.size()) - 3*chr.size() / 4) + chr.size() / 2;
+            chosenChromosome = chr.get(randomChromosomeIndex);
+
+        } else {
+
+            randomChromosomeIndex = rand.nextInt(chr.size() - 3 * chr.size() / 4) + 3 * chr.size() / 4;
+            chosenChromosome = chr.get(randomChromosomeIndex);
+
+        }
+        return chosenChromosome;
+    }
 }
